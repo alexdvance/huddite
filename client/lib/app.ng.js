0afb88ffc0f89ef02bf76479b88763c940ff999c
Alex Vance <alexdvance@gmail.com>
2015-11-12 23:10:53 -0500

    First commit: initialized app with generator-angular-meteor.

---
+++ client/lib/app.ng.js
@@ -0,0 +1,16 @@
+angular.module('hudditeApp', [
+  'angular-meteor',
+  'ui.router',
+  'ngMaterial',
+  'angularUtils.directives.dirPagination'
+]);
+
+onReady = function() {
+  angular.bootstrap(document, ['hudditeApp']);
+};
+  
+if(Meteor.isCordova) {
+  angular.element(document).on('deviceready', onReady);
+} else {
+  angular.element(document).ready(onReady);
+}
---
+++ client/main/main.routes.ng.js
@@ -0,0 +1,11 @@
+'use strict'
+
+angular.module('hudditeApp')
+.config(function($stateProvider) {
+  $stateProvider
+  .state('main', {
+    url: '/',
+    templateUrl: 'client/main/main.view.ng.html',
+    controller: 'MainCtrl'
+  });
+});
